2e66d81ebb97741c441ac7282b57fa1ebb67534b
Wei-Cheng Wu <spencerwu85@gmail.com>
2019-03-30 21:53:03 +0800

    Add menu

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,8 +1,8 @@
 
 function set_height() {
   var screen_height = $(window).height() - $("#mainMenu").height();
-  $("#indexCenter").css("height", screen_height * 0.4);
-  $("#lightGallery").css("height", screen_height * 0.6);
+  $("#indexCenter").css("height", screen_height * 0.5);
+  $("#lightGallery").css("height", screen_height * 0.5);
 }
 
 /*
