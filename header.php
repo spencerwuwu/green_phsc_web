24537d9d0f083ab14cb3ead4bb3442d484fb23ec
Wei-Cheng Wu <spencerwu85@gmail.com>
2019-04-08 12:59:43 +0800

    Migrate to php

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head lang="zh-TW">
+    <meta charset="utf-8">
+    <title>
+      綠攝影劇
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="javascripts/jquery-3.3.1.min.js"></script>
+    <script src="semantic-ui/semantic.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="semantic-ui/semantic.min.css">
+    <link type="text/css" rel="stylesheet" href="lg-dist/css/lightgallery.min.css" />
+    <link type="text/css" rel="stylesheet" href="slick/slick.css" />
+
+    <link rel="stylesheet" type="text/css" href="styles/main.css">
+    <link rel="stylesheet" type="text/css" href="styles/index.css">
+  </head>
+
+  <body>
+    <!-- Json2Html -->
+    <script src="javascripts/json2html.js"></script>
+
+    <!-- lightgallery -->
+    <script src="lg-dist/js/lightgallery-all.min.js"></script>
+    <script src="javascripts/jquery.mousewheel.min.js"></script>
+    <script src="slick/slick.min.js"></script>
+
+    <?php
+       $menu = file_get_contents('menu.html');
+       echo $menu;
+    ?>
+    
+    <div id="body-container">
+
---
+++ photo_live.php
@@ -0,0 +1,89 @@
+<?php include("header.php"); ?>
+
+<!-- Content -->
+<div id="content-container">
+
+  <!-- IG Feeds -->
+  <div class="my-header">
+    即時賽況
+  </div>
+  <div class="my-content">
+    <br/>
+
+    <div class="ui three column doubling stackable grid">
+      <div class="column">
+        <div class="ui centered card">
+          <div class="content">
+            <div class="right floated meta">14h</div>
+            <img class="ui avatar image" src="images/elliot.jpg"> Elliot
+          </div>
+          <div class="image">
+            <img src="images/flower-creature.jpg">
+          </div>
+          <div class="content">
+            <span class="right floated">
+              <i class="heart outline like icon"></i>
+              17 likes
+            </span>
+            <i class="comment icon"></i>
+            3 comments
+          </div>
+        </div>
+      </div>
+
+      <div class="column">
+        <div class="ui centered card">
+          <div class="content">
+            <div class="right floated meta">16h</div>
+            <img class="ui avatar image" src="images/elliot.jpg"> Elliot
+          </div>
+          <div class="image">
+            <img src="images/aesthetic-sakura-wallpaper.jpg">
+          </div>
+          <div class="content">
+            <span class="right floated">
+              <i class="heart outline like icon"></i>
+              17 likes
+            </span>
+            <i class="comment icon"></i>
+            3 comments
+          </div>
+        </div>
+      </div>
+
+      <div class="column">
+        <div class="ui centered card">
+          <div class="content">
+            <div class="right floated meta">19h</div>
+            <img class="ui avatar image" src="images/elliot.jpg"> Elliot
+          </div>
+          <div class="image">
+            <img src="images/aestetic-taipei.jpg">
+          </div>
+          <div class="content">
+            <span class="right floated">
+              <i class="heart outline like icon"></i>
+              17 likes
+            </span>
+            <i class="comment icon"></i>
+            3 comments
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <div class="my-content">
+  </div>
+
+</div>
+
+
+<!-- End of Content -->
+
+
+<?php
+   $footer = file_get_contents('footer.html');
+   echo $footer;
+?>
